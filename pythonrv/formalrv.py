5d29512bb75140747152095d4239f8e069672d75
adam <tgwizard@gmail.com>
2012-10-24 16:03:44 +0200

    An idea for formal verification
    An 'automata-based' formal verification approach. Trivial and ugly.
    Blaechchch!!!:...... :(

---
+++ pythonrv/formalrv.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+##################################################################
+### decorators
+##################################################################
+
+def formal_spec(spec):
+    return _make_machine(spec())
+
+
+##################################################################
+### the basic specification functions
+##################################################################
+
+def make_assert(*stms):
+    x = [_make_lambda(a) for a in stms]
+    return x
+
+
+def make_next(next):
+    if callable(next):
+        return [lambda e: (True, None, next())]
+    else:
+        return [lambda e: (True, None, next)]
+
+def make_if(exp=None, then=None, els=None):
+    assert not exp is None, "The exp parameter is required"
+    assert not then is None, "The then parameter is required"
+
+    exp_lambda = _make_lambda(exp)
+    then_lambda = _make_lambda(then)
+    else_lambda = _make_lambda(True if els is None else els)
+
+    return [lambda e: then_lambda(e) if exp_lambda(e) else else_lambda(e)]
+
+def _make_lambda(assertion):
+    if callable(assertion):
+        return assertion
+
+    try:
+        if callable(assertion[0]):
+            return assertion[0]
+    except:
+        pass
+
+    return lambda e: assertion
+
+##################################################################
+### the basic specification functions
+##################################################################
+
+class Machine(object):
+    def __init__(self, transitions):
+        self.transitions = transitions
+
+    def __call__(self, event):
+        new_transitions = []
+        errors = []
+        print "checking transitions: ", self.transitions
+
+        for t in self.transitions:
+            e, msg, new = _extract_transition_results(t, event)
+
+            print "asserting t: (%s, %s, %s)" % (e, msg, new)
+            if not e:
+                pass
+                errors.append((e, msg))
+            new_transitions += new
+
+        print "new transitions: ", new_transitions
+        self.transitions = new_transitions
+
+        for e, msg in errors:
+            assert e, msg
+
+
+def _extract_transition_results(transition, event):
+    result = transition(event)
+    print ">> result: ", result
+    e = None
+    msg = None
+    new = []
+    try:
+        e, msg, new = result
+    except:
+        try:
+            e, msg = result
+        except:
+            e = result
+    return e, msg, new
+
+def _make_machine(transitions):
+    return Machine(transitions)
---
+++ pythonrv/test/formalrv_test.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from pythonrv import rv
+from pythonrv.formalrv import formal_spec, make_assert, make_next, make_if
+
+class TestFormalRV(unittest.TestCase):
+    def test_assert_cases(self):
+        self.assertEquals(len(make_assert()), 0)
+
+        self.assertTrue(make_assert(True)[0](None))
+        self.assertFalse(make_assert(False)[0](None))
+
+        self.assertTrue(make_assert(lambda e: True)[0](None))
+        self.assertFalse(make_assert(lambda e: False)[0](None))
+
+        a = make_assert(True, False, lambda e: False, lambda e: e != 5)
+        self.assertEquals(len(a), 4)
+        self.assertTrue(a[0](None))
+        self.assertFalse(a[1](None))
+        self.assertFalse(a[2](None))
+
+        self.assertTrue(a[3](None))
+        self.assertTrue(a[3](4))
+        self.assertFalse(a[3](5))
+
+    def test_basic_assert(self):
+        class M(object):
+            def m(self, x):
+                pass
+
+        @rv.monitor(m=M.m)
+        @formal_spec
+        def spec():
+            return make_assert(True, False, 5 == 3)
+
+        a = M()
+        with self.assertRaises(AssertionError):
+            a.m(1)
+        # second time no error, since we have no loop in the specification
+        a.m(1)
+
+    def test_basic_assert_with_message(self):
+        class M(object):
+            def m(self, x):
+                pass
+
+        @rv.monitor(m=M.m)
+        @formal_spec
+        def spec():
+            return make_assert((False, "buffy"))
+
+        a = M()
+        with self.assertRaises(AssertionError) as e:
+            a.m(1)
+        self.assertEquals(e.exception.message, "buffy")
+        a.m(1)
+
+    def test_basic_assert_with_different_inputs(self):
+        for i in range(5):
+            class M(object):
+                def m(self, x):
+                    pass
+
+            @rv.monitor(m=M.m)
+            @formal_spec
+            def spec2():
+                return make_assert(lambda e: e.fn.m.inputs[1] != 2)
+
+            a = M()
+            if i == 2:
+                with self.assertRaises(AssertionError):
+                    a.m(i)
+            a.m(-1)
+            a.m(123)
+
+    def test_next(self):
+        class M(object):
+            def m(self, x):
+                pass
+
+        @rv.monitor(m=M.m)
+        @formal_spec
+        def spec():
+            a = make_assert(False)
+            return make_next(a)
+
+        a = M()
+        a.m(1)
+        with self.assertRaises(AssertionError):
+            a.m(1)
+        a.m(1)
+
+    def test_more_assert_with_next_loop(self):
+        class M(object):
+            def m(self, x):
+                pass
+
+        @rv.monitor(m=M.m)
+        @formal_spec
+        def spec():
+            asserts = make_assert(True,
+                    lambda e: e.fn.m.inputs[1] > 0,
+                    lambda e: e.fn.m.inputs[1] != 5,
+                    make_next(lambda: asserts))
+
+            return asserts
+
+        a = M()
+
+        a.m(1)
+        a.m(2)
+        with self.assertRaises(AssertionError):
+            a.m(0)
+        a.m(1337)
+        with self.assertRaises(AssertionError):
+            a.m(5)
+        a.m(98789374)
+        with self.assertRaises(AssertionError):
+            a.m(5)
+        with self.assertRaises(AssertionError):
+            a.m(-123487689)
+
+    def test_simple_if(self):
+        class M(object):
+            def m(self, x):
+                pass
+
+        @rv.monitor(m=M.m)
+        @formal_spec
+        def spec():
+            return make_if(True, make_assert(False))
+
+        a = M()
+        with self.assertRaises(AssertionError):
+            a.m(0)
+        a.m(0)
+        a.m(0)
+        a.m(0)
+
+    def test_if_required_attributes(self):
+        with self.assertRaises(AssertionError):
+            make_if()
+        with self.assertRaises(AssertionError):
+            make_if(True)
+        with self.assertRaises(AssertionError):
+            make_if(exp=False)
+        with self.assertRaises(AssertionError):
+            make_if(then=False)
+        with self.assertRaises(AssertionError):
+            make_if(els=False)
+        with self.assertRaises(AssertionError):
+            make_if(then=False,els=False)
+        with self.assertRaises(AssertionError):
+            make_if(exp=False,els=False)
+
+    def test_if_cases(self):
+        self.assertTrue(make_if(exp=True,then=True)[0](None))
+        self.assertTrue(make_if(True,True)[0](None))
+
+        self.assertTrue(make_if(exp=True,then=True,els=False)[0](None))
+        self.assertTrue(make_if(True,True,False)[0](None))
+
+        self.assertFalse(make_if(exp=True,then=False)[0](None))
+        self.assertFalse(make_if(exp=True,then=False,els=True)[0](None))
+
+        self.assertTrue(make_if(exp=False,then=False)[0](None))
+        self.assertTrue(make_if(exp=False,then=False,els=True)[0](None))
+        self.assertFalse(make_if(exp=False,then=True,els=False)[0](None))
+
+    def test_simple_if_else(self):
+        for i in range(2):
+            class M(object):
+                def m(self, x):
+                    pass
+
+            @rv.monitor(m=M.m)
+            @formal_spec
+            def spec():
+                return make_if(lambda e: e.fn.m.inputs[1] == 0,
+                        make_assert((False,"heisenberg0")),
+                        make_assert((False,"heisenberg1")))
+
+            a = M()
+            with self.assertRaises(AssertionError) as e:
+                a.m(i)
+            self.assertEquals(e.exception.message, "heisenberg%d" % i)
+
+            a.m(0)
+            a.m(0)
+            a.m(0)
